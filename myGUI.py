a574f44eb1c2f1b5479921d3916a8706237fa52f
Parinyawat Saenthaweesuk <67275396+JustKeepPlay@users.noreply.github.com>
2023-12-07 19:47:48 +0700

    update on predict graph

--- myGUI.py
+++ myGUI.py
@@ -8,21 +8,19 @@ import networkx as nx
 
 doc = doc_graph(5, 'wrand')
 
-
 # ------------------------------------------------------------------------------------------------------------
 
 def create_learning_page(notebook):
     global learning_page, entry, create_line_graph, result_label
     learning_page = tk.Frame(notebook)
     notebook.add(learning_page, text="Learning Phase")
-    #def fibo (n):
-    #    if n <=1:
-    #        return n
-    #    else:
-    #        return fibo(n-1)+fibo(n-2)
-    #fiboNum = [fibo(i) for i in range(30)]
-    #fiboNum = [1,1,1,2,1,3,1,3,1,4,1,5]
-    #doc.add_doc(fiboNum)    
+    # def fibo (n):
+    #     if n <=1:
+    #         return n
+    #     else:
+    #         return fibo(n-1)+fibo(n-2)
+    # fiboNum = [fibo(i) for i in range(30)]
+    # doc.add_doc(fiboNum)    
 
     upload_button = tk.Button(learning_page, text="Upload File", command=upload_file)
     upload_button.pack()
@@ -31,6 +29,7 @@ def create_learning_page(notebook):
     input_label.pack()
 
     entry = tk.Entry(learning_page)
+    # entry.insert(0, "1,2,3")
     entry.pack()
 
     calculate_button = tk.Button(learning_page, text="Train", command=get_numbers)
@@ -70,9 +69,9 @@ def create_line_graph(numbers, ax=None, canvas=None):
 def get_numbers():
     input_values = entry.get()
     numbers = [int(num) for num in input_values.split(',')]
-    #output = doc.gen_next(numbers, 5)
-    doc.add_doc(numbers,5)
-    result_label.config(text=f"Result: {numbers}")
+    train(numbers, 5)
+    # output = doc.gen_next(numbers, 5)
+    result_label.config(text=f"Result: { numbers }")
     create_line_graph(numbers, ax=create_line_graph.ax, canvas=create_line_graph.canvas)
 
 def upload_file():
@@ -82,6 +81,10 @@ def upload_file():
             content = file.read()
             entry.delete(0, tk.END)
             entry.insert(0, content)
+
+def train(seq, h):
+    doc.add_doc(seq,h)  
+
 
 # ------------------------------------------------------------------------------------------------------------
 
@@ -107,11 +110,12 @@ def get_pred_num():
     nums = [int(num) for num in pred_input.split(',')]
     #print(nums)
     #output = doc.gen_next(numbers, 5)
-    gen_num = nums
+    gen_num = nums.copy()
     pred_num = []
     pred_num.append(doc.gen_next(gen_num,5))
-    
-    create_pred_graph(nums, pred_num , ax=create_pred_graph.ax, canvas=create_pred_graph.canvas)
+    print('nums :',nums)
+    print('gen nums :',gen_num)
+    create_pred_graph(nums.copy(), pred_num , ax=create_pred_graph.ax, canvas=create_pred_graph.canvas)
     
 
 def create_pred_graph(numbers, predicted_number, ax=None, canvas=None):
@@ -120,17 +124,17 @@ def create_pred_graph(numbers, predicted_number, ax=None, canvas=None):
     
     ax.clear()  # Clear the previous plot
      # Plot the input numbers in blue
-    print(predicted_number)
+    print(numbers)
 
-    ax.plot(range(len(numbers)-1), numbers[:-1], marker='o', color='blue', label='Input Numbers')
+    ax.plot(range(len(numbers)), numbers, marker='o', color='blue', label='Input Numbers')
     
     # Plot only the last element of the predicted number in red
-    ax.plot(len(numbers)-1 , predicted_number[0][-1], marker='o', color='red', label='Predicted Number')
+    ax.plot(len(numbers) , predicted_number[0][-1], marker='o', color='red', label='Predicted Number')
 
-    for x, y in zip(range(len(numbers)-1),numbers[:-1]):
+    for x, y in zip(range(len(numbers)),numbers):
         plt.text(x, y, f'{y}', ha='right', va='bottom',c='blue')
     
-    plt.text(len(numbers)-1, predicted_number[0][-1], f'{predicted_number[0][-1]}', ha='left', va='bottom', c='red')
+    plt.text(len(numbers), predicted_number[0][-1], f'{predicted_number[0][-1]}', ha='left', va='bottom', c='red')
 
     # Set x-axis locator to integer values
     ax.locator_params(axis='y', integer=True)
@@ -157,53 +161,106 @@ def create_pred_graph(numbers, predicted_number, ax=None, canvas=None):
   
 
 def create_evaluation_page(notebook):
-    global evaluation_page
+    global evaluation_page, eva_node_entry, eva_friends_entry
     evaluation_page = tk.Frame(notebook)
     notebook.add(evaluation_page, text="Evaluation Phase")
+
+    eva_node_label = tk.Label(evaluation_page, text="Node: ")
+    eva_node_label.pack()
+    eva_node_entry = tk.Entry(evaluation_page)
+    eva_node_entry.pack()
+    # eva_friends_label = tk.Label(evaluation_page, text="Neighbour: ")
+    # eva_friends_label.pack()
+    # eva_friends_entry = tk.Entry(evaluation_page)
+    # eva_friends_entry.pack()
 
     # Create a button to trigger the Evaluation on the evaluation page
     evaluation_button = tk.Button(evaluation_page, text="Evaluate", command=create_network)
     evaluation_button.pack()
 
+    # Initialize canvas variable
+    create_evaluation_page.canvas = None
+    create_evaluation_page.toolbar = None
+
+# def get_node_edges(graph, node):
+#     if graph.has_node(node):
+#         edges = list(graph.edges(node))
+#         return edges
+#     else:
+#         return None
+
 
 def create_network():
+    specified_node = eva_node_entry.get()
     G = nx.DiGraph()
     G.add_edges_from(doc.get_edge_table())
-
-    # Clear the previous network graph (if any)
-    if hasattr(create_network, 'canvas'):
-        create_network.canvas.get_tk_widget().destroy()
 
     # Create a new Matplotlib figure for the network graph
     fig, ax = plt.subplots()
     ax.set_title('Network Graph')
 
-    pos = nx.kamada_kawai_layout(G)
+    # Clear the previous network graph (if any)
+    if create_evaluation_page.canvas:
+        create_evaluation_page.canvas.get_tk_widget().destroy()
+    if create_evaluation_page.toolbar:
+        create_evaluation_page.toolbar.destroy()
 
-    # Draw the network graph
-    nx.draw(
-        G,
-        pos,
-        with_labels=True,
-        node_color='lightgreen'
-    )
+
+    try:
+        if len(eva_node_entry.get()) == 0:
+            pos = nx.kamada_kawai_layout(G)
+            # Draw the network graph
+            nx.draw(
+                G,
+                pos,
+                with_labels=True,
+                node_color='lightgreen'
+            )
+        else:
+            specified_node = int(specified_node)
+            if G.has_node(specified_node):
+                pos = nx.spring_layout(G)
+                # node_edges = get_node_edges(G, specified_node)
+                # print(f"Edges connected to Node {specified_node}: {node_edges}")
+
+                # # Create a subgraph with the specified edges
+                # subgraph_edges = [(specified_node, neighbor) for neighbor in G.neighbors(specified_node)]
+                # subgraph = G.subgraph(subgraph_edges)
+                subgraph = nx.ego_graph(G, specified_node)
+
+                # Draw the network graph
+                nx.draw(
+                    subgraph,
+                    pos,
+                    with_labels=True,
+                    node_color='lightgreen'
+                )
+            else:
+                ax.set_title('No node specified.')
+            
+    except:
+        print("An exception error")
 
     # Embed the Matplotlib network graph in the Tkinter window
     canvas = FigureCanvasTkAgg(fig, master=evaluation_page)
+    create_evaluation_page.canvas = canvas  # Store canvas for future destruction
     canvas_widget = canvas.get_tk_widget()
     canvas_widget.pack()
 
     # Add the Matplotlib NavigationToolbar2Tk
     toolbar = NavigationToolbar2Tk(canvas, evaluation_page)
+    create_evaluation_page.toolbar = toolbar  # Store toolbar for future destruction
     toolbar.update()
     canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    
 
 # ------------------------------------------------------------------------------------------------------------
 
 
 def main():
     window = tk.Tk()
-    window.geometry('800x600')
+    window.geometry('800x700')
     window.title("Graph Learning GUI ")
 
     notebook = ttk.Notebook(window)
